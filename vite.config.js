b983a18789ed25c7aaf909e516c213049f8622ca
Zohaib Khan | Front-End Web Developer <zk3165657513@gmail.com>
2026-03-13 23:20:19 +0500

    remove base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project2/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
